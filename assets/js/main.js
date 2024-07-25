a90d6c9805a9a073e6ea23ccfa6c588fe4fb3822
brunocriacoes <br.rafael@outlook.com>
2024-07-25 12:22:31 -0300

    fix

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    autoplay: {
+        delay: 5000,
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
